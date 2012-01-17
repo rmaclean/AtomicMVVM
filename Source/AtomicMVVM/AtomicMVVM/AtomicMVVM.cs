121c4738db6273232e0b723bdb4ea5027a6a4f38
Robert MacLean <robert@sadev.co.za>
2012-01-17 18:45:21 +0200

    Initial checkin

---
+++ Source/AtomicMVVM/AtomicMVVM/AtomicMVVM.cs
@@ -0,0 +1,171 @@
+﻿
+namespace AtomicMVVM
+{
+    using System;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Reflection;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Input;
+
+    public class Bootstrapper<TShell, TContent>
+        where TShell : IShell
+        where TContent : CoreData
+    {
+        private IShell shell;
+        private CoreData viewModel;
+        private UserControl view;
+
+        public Bootstrapper()
+        {
+            shell = (IShell)typeof(TShell).GetConstructor(Type.EmptyTypes).Invoke(null);
+
+            this.ChangeView<TContent>();
+
+            (shell as Window).Show();
+        }
+
+        public void ChangeView<TNewContent>()
+            where TNewContent : CoreData
+        {
+            viewModel = typeof(TNewContent).GetConstructor(Type.EmptyTypes).Invoke(null) as CoreData;
+            var viewName = typeof(TNewContent).FullName.Replace(".ViewModels.", ".Views.");
+            view = Type.GetType(viewName, true, true).GetConstructor(Type.EmptyTypes).Invoke(null) as UserControl;
+
+            var validMethods = (from m in viewModel.GetType().GetMethods()
+                                where m.IsPublic &&
+                                     !m.IsSpecialName &&
+                                     !m.Attributes.HasFlag(MethodAttributes.VtableLayoutMask) &&
+                                     m.ReturnType == typeof(void)
+                                select m).ToList();
+
+            foreach (var method in validMethods)
+            {
+                var control = view.FindName(method.Name);
+                if (control != null && control is ICommandSource)
+                {
+                    var commandProperty = control.GetType().GetProperty("Command");
+                    if (commandProperty.GetValue(control) == null)
+                    {
+                        var commandParameterProperty = control.GetType().GetProperty("CommandParameter");
+                        if (commandProperty.CanWrite && commandParameterProperty.CanWrite)
+                        {
+                            var canExecuteExists = false;
+                            var canExecuteMethod = viewModel.GetType().GetMethod("Can" + method.Name, Type.EmptyTypes);
+                            if (canExecuteMethod != null)
+                            {
+                                canExecuteExists = canExecuteMethod.ReturnType == typeof(bool);
+                            }
+
+                            var command = new AttachedCommand(method.Name, canExecuteExists);
+
+                            if (canExecuteMethod != null)
+                            {
+                                foreach (var attribute in canExecuteMethod.GetCustomAttributes<RelatedPropertyAttribute>())
+                                {
+                                    viewModel.PropertyChanged += (s, e) =>
+                                        {
+                                            if (e.PropertyName == attribute.PropertyName)
+                                            {
+                                                command.RaiseCanExecuteChanged();
+                                            }
+                                        };
+                                }
+                            }
+
+                            commandProperty.SetValue(control, command);
+                            commandParameterProperty.SetValue(control, viewModel);
+                        }
+                    }
+                }
+            }
+
+            view.DataContext = viewModel;
+
+            shell.ChangeContent(view);
+        }
+    }
+
+    public class AttachedCommand : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            if (!canExecuteExists)
+            {
+                return true;
+            }
+
+            if (parameter == null)
+            {
+                return false;
+            }
+
+            if (canExecuteMethod == null)
+            {
+                canExecuteMethod = parameter.GetType().GetMethod("Can" + this.methodName, Type.EmptyTypes);
+            }
+
+            return (bool)canExecuteMethod.Invoke(parameter, null);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            if (CanExecuteChanged != null)
+            {
+                CanExecuteChanged(this, new EventArgs());
+            }
+        }
+
+        public event EventHandler CanExecuteChanged;
+        private string methodName;
+        private MethodInfo executeMethod;
+        private MethodInfo canExecuteMethod;
+        private bool canExecuteExists;
+
+        public AttachedCommand(string methodName, bool canExecuteExists)
+        {
+            this.methodName = methodName;
+            this.canExecuteExists = canExecuteExists;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (executeMethod == null)
+            {
+                executeMethod = parameter.GetType().GetMethod(this.methodName);
+            }
+
+            executeMethod.Invoke(parameter, null);
+        }
+    }
+
+    public class CoreData : INotifyPropertyChanged
+    {
+        public void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    [System.AttributeUsage(System.AttributeTargets.Method, Inherited = false, AllowMultiple = true)]
+    sealed class RelatedPropertyAttribute : System.Attribute
+    {
+        public RelatedPropertyAttribute(string propertyName)
+        {
+            this.PropertyName = propertyName;
+        }
+
+        public string PropertyName { get; private set; }
+    }
+
+    public interface IShell
+    {
+        void ChangeContent(UserControl viewContent);
+    }
+}
---
+++ Source/AtomicMVVM/AtomicMVVM/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AtomicMVVM")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Robert MacLean")]
+[assembly: AssemblyProduct("AtomicMVVM")]
+[assembly: AssemblyCopyright("Copyright © Robert MacLean 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly:System.CLSCompliant(true)]
+[assembly:System.Resources.NeutralResourcesLanguage("en")]
+[assembly: ComVisible(false)]
+[assembly: Guid("dd623e44-e327-437d-8462-00676ccd42f0")]
+[assembly: System.Security.SecurityRules(System.Security.SecurityRuleSet.Level2)]
+//http://www.semver.org/
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
