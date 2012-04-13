7280344c48c1a8f94de20a63176fbaeca07b0e91
Robert MacLean <robert@sadev.co.za>
2012-04-13 12:49:01 +0200

    Added a demo for Windows Phone 7.1 using pure atomicmvvm

---
+++ Demos/Windows Phone 7.1 Demo - Pure AtomicMVVM/WP71Demo/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿
+namespace WP71Demo
+{
+    using System.Windows.Controls;
+    using AtomicMVVM;
+    using Microsoft.Phone.Controls;
+
+    public partial class MainPage : PhoneApplicationPage, IShell
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void ChangeContent(UserControl viewContent)
+        {
+            ContentPanel.Content = viewContent;
+        }
+    }
+}
---
+++ Demos/Windows Phone 7.1 Demo - Pure AtomicMVVM/WP71Demo/ViewModels/Main.cs
@@ -0,0 +1,47 @@
+﻿
+namespace WP71Demo.ViewModels
+{
+    using AtomicMVVM;
+
+    public class Main : CoreData
+    {
+        private int position = -1;
+        private string[] messages = new[]{
+            "These are not the droids you are looking for",
+            "I love you - I know",
+            "May the force be with you",
+            "Made the kessel run in 12 parsecs"
+        };
+
+        private string message;
+
+        public string Message
+        {
+            get { return message; }
+            set
+            {
+                if (value != message)
+                {
+                    message = value;
+                    RaisePropertyChanged("Message");
+                }
+            }
+        }
+
+        public Main()
+        {
+            CycleMessages();
+        }
+
+        public void CycleMessages()
+        {
+            position++;
+            if (position == messages.Length)
+            {
+                position = 0;
+            }
+
+            Message = messages[position];
+        }
+    }
+}
