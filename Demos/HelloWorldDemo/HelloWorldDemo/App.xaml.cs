aac9e16d2ad00543772bf73e6a371e4844f0e717
Robert MacLean <robert@sadev.co.za>
2012-04-13 12:19:49 +0200

    Updated Hello World demo to latest AtomicMVVM bits

--- Demos/HelloWorldDemo/HelloWorldDemo/App.xaml.cs
+++ Demos/HelloWorldDemo/HelloWorldDemo/App.xaml.cs
@@ -1,14 +1,21 @@
-﻿using System.Windows;
-using AtomicMVVM;
-using HelloWorldDemo.ViewModels;
+﻿
 
 namespace HelloWorldDemo
 {
+    using System.Windows;
+    using AtomicMVVM;
+    using HelloWorldDemo.ViewModels;
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
         public Bootstrapper<MainWindow, HelloWorld> Bootstrapper = new Bootstrapper<MainWindow,HelloWorld>();
+
+        public App()
+        {
+            Bootstrapper.Start();
+        }
     }
 }
