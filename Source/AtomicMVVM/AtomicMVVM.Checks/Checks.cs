3487d5351796047f2dbce044320aed1e748305f8
Robert MacLean <robert@sadev.co.za>
2012-07-08 20:49:09 +0200

    Work on unit testing for checks & XMLDocs

---
+++ Source/AtomicMVVM/AtomicMVVM.Checks/Checks.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AtomicMVVM
+{
+    public class Checks
+    {
+        [Fact]
+        public void BootStrapperConstructor()
+        {
+            var bootStrapper = new Bootstrapper();
+            Assert.IsType<Bootstrapper>(bootStrapper);
+        }
+    }
+}
